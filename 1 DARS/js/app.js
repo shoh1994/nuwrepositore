7bb29fa7f7b7f5077958ade10ad1515a5cfd7748
shohrux <nurmatovshohruhbek1@gmail.com>
2023-07-26 13:40:51 +0500

    first commit

---
+++ 1 DARS/js/app.js
@@ -0,0 +1,18 @@
+// const shoh = (7 + 5)
+// console.log(shoh);
+
+// let a = 1;
+// let b = 2;
+// let c = a;
+// a = b;
+// b = c
+
+// console.log('a' , a);
+// console.log('b', b);
+
+// console.log(5*5===25);
+
+const isAge = true;
+const isclose = flase;
+
+console.log(isAge || isclose);
